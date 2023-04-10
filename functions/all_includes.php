b79e4c3e1f814670154c609fd5879e8905c58053
LightBurdenOfficial <francis.santiago@lightburden.net>
2023-04-10 17:13:23 -0300

    [NEW] Arquivo de inclusão de funções, cabeçalhos e requisições php

---
+++ functions/all_includes.php
@@ -0,0 +1,27 @@
+<?php
+if ($_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])){
+	header( 'HTTP/1.0 404 Forbidden', TRUE, 404 );
+	die(header('location: /dashboard'));
+}
+
+/*
+* Cabeçalhos
+*/
+$path = $_SERVER['DOCUMENT_ROOT'];
+date_default_timezone_set('America/Sao_Paulo');
+error_reporting(E_ERROR | E_PARSE | E_ALL);
+
+require ($path.'/functions/dbConnection.php');
+require ($path.'/functions/vars.php');
+
+if(isset($_SESSION['session_userLogin'])){
+	$userSessionId = $_SESSION['session_userLogin'];
+}
+
+/*
+* ==================================================
+* Funções
+* ==================================================
+*/
+require ($path.'/functions/terms/general.php');
+?>
