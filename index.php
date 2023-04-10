469dc4092e12b1f064f447e81c48b8ab0e0e3068
LightBurdenOfficial <francis.santiago@lightburden.net>
2023-04-10 16:08:36 -0300

    [NEW] Rotas iniciais de login

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+	if(session_status() == PHP_SESSION_NONE){
+		session_start();
+	}
+	if (!isset($_SESSION['userLogin'])) {
+		header('location: login');
+	} else {
+		header('location: dashboard');
+	}
+?>
