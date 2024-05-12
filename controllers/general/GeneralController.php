dcb9c9e439216299c48bb369dc38143197d1a4e8
Francis Santiago <francis1505@gmail.com>
2024-05-12 15:54:34 -0300

    [FEAT] Função de adicionar dados em uma tabela no banco de dados

--- controllers/general/GeneralController.php
+++ controllers/general/GeneralController.php
@@ -86,5 +86,57 @@ class GeneralController {
         
         return $msg;
     }
+
+    /**
+     * Insere dados em uma tabela do banco de dados.
+     *
+     * Esta função executa uma consulta SQL para inserir dados em uma tabela
+     * do banco de dados, com base nas colunas e valores fornecidos.
+     *
+     * @param string $table O nome da tabela do banco de dados.
+     * @param array $columns Um array contendo os nomes das colunas a serem inseridas.
+     * @param array $values Um array contendo os valores a serem inseridos.
+     * @param string $bindTypes Uma string contendo os tipos dos parâmetros a serem vinculados (por exemplo, "sss" para três strings).
+     * @param mixed $bindParams Um ou mais parâmetros a serem vinculados à consulta preparada.
+     *
+     * @return array Um array associativo contendo o código de status e a mensagem da operação.
+     *               O código de status pode ser um dos seguintes:
+     *               - 200: OK (inserção bem-sucedida).
+     *               - 412: Pré-condição falhou (erro ao preparar a consulta).
+     *               - 500: Erro interno do servidor (erro ao executar a consulta).
+     */
+    public function insertDataInTable($table, $columns, $values, $bindTypes, $bindParams){
+        $msg = [];
+        // Construir a string SQL
+        $sql = "INSERT INTO $table (" . implode(', ', $columns) . ") VALUES (" . implode(', ', $values) . ")";
+        $stmt = $this->conn->prepare($sql);
+    
+        if($stmt === false){
+            $msg = [
+                'code' => 412,
+                'message' => "Erro ao preparar inserção de dados no banco de dados."
+            ];
+        } else {
+            // Vincular os parâmetros
+            $stmt->bind_param($bindTypes, ...$bindParams);
+    
+            if($stmt->execute()){
+                $msg = [
+                    'code' => 200,
+                    'message' => "Dados inseridos com sucesso!"
+                ];
+            } else {
+                $msg = [
+                    'code' => 500,
+                    'message' => "Erro ao executar inserção de dados no banco de dados."
+                ];
+            }
+    
+            $stmt->free_result();
+            $stmt->close();
+        }
+        
+        return $msg;
+    }
 }
 ?>
