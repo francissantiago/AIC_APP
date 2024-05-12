e71faa6aab416460916ba8331da3b33c9f6899b6
Francis Santiago <francis1505@gmail.com>
2024-05-12 15:50:38 -0300

    [DOCS][GeneralController] PoliciesController

--- controllers/policies/PoliciesController.php
+++ controllers/policies/PoliciesController.php
@@ -8,7 +8,19 @@ class PoliciesController extends GeneralController {
         $this->conn = $conn;
     }
 
-	/* Recuperaçãos dos dados da tabela `user_policies` */
+	/**
+	 * Obtém todas as políticas de usuário do banco de dados.
+	 *
+	 * Esta função executa uma consulta SQL para recuperar todas as políticas de usuário
+	 * armazenadas na tabela "user_policies" do banco de dados.
+	 *
+	 * @return array Um array associativo contendo o código de status e a mensagem da operação.
+	 *               O código de status pode ser um dos seguintes:
+	 *               - 200: OK (consulta bem-sucedida).
+	 *               - 404: Não encontrado (nenhum dado corresponde à consulta).
+	 *               - 412: Pré-condição falhou (erro ao preparar a consulta).
+	 *               - 500: Erro interno do servidor (erro ao executar a consulta).
+	 */
 	public function get_userPolicies() {
 		$tableName = "user_policies";
 
@@ -18,7 +30,19 @@ class PoliciesController extends GeneralController {
 		return $getResponse;
 	}
 
-	/* Recuperaçãos dos dados da tabela `user_terms` */
+	/**
+	 * Obtém todas os termos de usuário do banco de dados.
+	 *
+	 * Esta função executa uma consulta SQL para recuperar todos os termos de usuário
+	 * armazenados na tabela "user_terms" do banco de dados.
+	 *
+	 * @return array Um array associativo contendo o código de status e a mensagem da operação.
+	 *               O código de status pode ser um dos seguintes:
+	 *               - 200: OK (consulta bem-sucedida).
+	 *               - 404: Não encontrado (nenhum dado corresponde à consulta).
+	 *               - 412: Pré-condição falhou (erro ao preparar a consulta).
+	 *               - 500: Erro interno do servidor (erro ao executar a consulta).
+	 */
 	public function get_userTerms() {
 		$tableName = "user_terms";
 
