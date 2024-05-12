53611516cff80ffec558d2155194e863acf84984
Francis Santiago <francis1505@gmail.com>
2024-05-12 16:00:43 -0300

    [REBRAND] Alteração de nomes de variáveis para tornar mais claro o propósito de cada uma

--- controllers/general/GeneralController.php
+++ controllers/general/GeneralController.php
@@ -15,11 +15,11 @@ class GeneralController {
      * limitação aos resultados da consulta.
      *
      * @param string $tableName O nome da tabela do banco de dados.
-     * @param string|null $auxColum O nome da coluna para a condição WHERE (opcional).
-     * @param mixed $auxParameter O parâmetro para a condição WHERE (opcional).
-     * @param string|null $auxParameterType O tipo do parâmetro para a condição WHERE (opcional).
-     * @param int|null $auxLimit O número máximo de linhas a serem retornadas (opcional).
-     * @param string|null $auxOrder A cláusula ORDER BY para ordenar os resultados (opcional).
+     * @param string|null $whereColumn O nome da coluna para a condição WHERE (opcional).
+     * @param mixed $whereValue O valor para a condição WHERE (opcional).
+     * @param string|null $whereValueType O tipo do valor para a condição WHERE (opcional).
+     * @param int|null $limit O número máximo de linhas a serem retornadas (opcional).
+     * @param string|null $orderBy A cláusula ORDER BY para ordenar os resultados (opcional).
      *
      * @return array Um array associativo contendo o código de status e a mensagem da operação.
      *               O código de status pode ser um dos seguintes:
@@ -28,23 +28,23 @@ class GeneralController {
      *               - 412: Pré-condição falhou (erro ao preparar a consulta).
      *               - 500: Erro interno do servidor (erro ao executar a consulta).
      */
-    public function selectAllDataInTable($tableName, $auxColum = null, $auxParameter = null, $auxParameterType = null, $auxLimit = null, $auxOrder = null) {
+    public function selectAllDataInTable($tableName, $whereColumn = null, $whereValue = null, $whereValueType = null, $limit = null, $orderBy = null) {
         $msg = [];
         $sql = "SELECT * FROM $tableName";
         
-        // Adiciona condição WHERE se $auxColum não for nulo
-        if ($auxColum !== null) {
-            $sql .= " WHERE $auxColum = ?";
+        // Adiciona condição WHERE se $whereColumn não for nulo
+        if ($whereColumn !== null) {
+            $sql .= " WHERE $whereColumn = ?";
         }
 
-        // Adiciona condição ORDER BY se $auxOrder não for nulo
-        if ($auxOrder !== null) {
-            $sql .= " ORDER BY $auxOrder";
+        // Adiciona condição ORDER BY se $orderBy não for nulo
+        if ($orderBy !== null) {
+            $sql .= " ORDER BY $orderBy";
         }
         
-        // Adiciona condição LIMIT se $auxLimit não for nulo
-        if ($auxLimit !== null) {
-            $sql .= " LIMIT $auxLimit";
+        // Adiciona condição LIMIT se $limit não for nulo
+        if ($limit !== null) {
+            $sql .= " LIMIT $limit";
         }
         
         $stmt = $this->conn->prepare($sql);
@@ -55,9 +55,9 @@ class GeneralController {
                 'message' => "Erro ao preparar seleção de informações no banco de dados."
             ];
         } else {
-            if ($auxColum !== null) {
-                // Se $auxColum não for nulo, bind do parâmetro
-                $stmt->bind_param($auxParameterType, $auxParameter);
+            if ($whereColumn !== null) {
+                // Se $whereColumn não for nulo, bind do parâmetro
+                $stmt->bind_param($whereValueType, $whereValue);
             }
         
             if ($stmt->execute()) {
@@ -87,13 +87,14 @@ class GeneralController {
         return $msg;
     }
 
+
     /**
      * Insere dados em uma tabela do banco de dados.
      *
      * Esta função executa uma consulta SQL para inserir dados em uma tabela
      * do banco de dados, com base nas colunas e valores fornecidos.
      *
-     * @param string $table O nome da tabela do banco de dados.
+     * @param string $tableName O nome da tabela do banco de dados.
      * @param array $columns Um array contendo os nomes das colunas a serem inseridas.
      * @param array $values Um array contendo os valores a serem inseridos.
      * @param string $bindTypes Uma string contendo os tipos dos parâmetros a serem vinculados (por exemplo, "sss" para três strings).
@@ -105,10 +106,10 @@ class GeneralController {
      *               - 412: Pré-condição falhou (erro ao preparar a consulta).
      *               - 500: Erro interno do servidor (erro ao executar a consulta).
      */
-    public function insertDataInTable($table, $columns, $values, $bindTypes, $bindParams){
+    public function insertDataInTable($tableName, $columns, $values, $bindTypes, $bindParams){
         $msg = [];
         // Construir a string SQL
-        $sql = "INSERT INTO $table (" . implode(', ', $columns) . ") VALUES (" . implode(', ', $values) . ")";
+        $sql = "INSERT INTO $tableName (" . implode(', ', $columns) . ") VALUES (" . implode(', ', $values) . ")";
         $stmt = $this->conn->prepare($sql);
     
         if($stmt === false){
