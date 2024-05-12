ae11bca0e50a25d9f19c496df406b2f1541f4580
Francis Santiago <francis1505@gmail.com>
2024-05-12 15:50:51 -0300

    Revert "[DOCS][GeneralController] PoliciesController"
    This reverts commit e71faa6aab416460916ba8331da3b33c9f6899b6.

--- controllers/policies/PoliciesController.php
+++ controllers/policies/PoliciesController.php
@@ -8,19 +8,7 @@ class PoliciesController extends GeneralController {
         $this->conn = $conn;
     }
 
-	/**
-	 * Obtém todas as políticas de usuário do banco de dados.
-	 *
-	 * Esta função executa uma consulta SQL para recuperar todas as políticas de usuário
-	 * armazenadas na tabela "user_policies" do banco de dados.
-	 *
-	 * @return array Um array associativo contendo o código de status e a mensagem da operação.
-	 *               O código de status pode ser um dos seguintes:
-	 *               - 200: OK (consulta bem-sucedida).
-	 *               - 404: Não encontrado (nenhum dado corresponde à consulta).
-	 *               - 412: Pré-condição falhou (erro ao preparar a consulta).
-	 *               - 500: Erro interno do servidor (erro ao executar a consulta).
-	 */
+	/* Recuperaçãos dos dados da tabela `user_policies` */
 	public function get_userPolicies() {
 		$tableName = "user_policies";
 
@@ -30,19 +18,7 @@ class PoliciesController extends GeneralController {
 		return $getResponse;
 	}
 
-	/**
-	 * Obtém todas os termos de usuário do banco de dados.
-	 *
-	 * Esta função executa uma consulta SQL para recuperar todos os termos de usuário
-	 * armazenados na tabela "user_terms" do banco de dados.
-	 *
-	 * @return array Um array associativo contendo o código de status e a mensagem da operação.
-	 *               O código de status pode ser um dos seguintes:
-	 *               - 200: OK (consulta bem-sucedida).
-	 *               - 404: Não encontrado (nenhum dado corresponde à consulta).
-	 *               - 412: Pré-condição falhou (erro ao preparar a consulta).
-	 *               - 500: Erro interno do servidor (erro ao executar a consulta).
-	 */
+	/* Recuperaçãos dos dados da tabela `user_terms` */
 	public function get_userTerms() {
 		$tableName = "user_terms";
 
