9597aff8850e6a4d4b9bc1bdbbeb5e86596e15a7
Francis Santiago <francis1505@gmail.com>
2024-06-01 13:21:17 -0300

    [FEAT] phpdotenv
    Armazenar a configuração no ambiente é um dos princípios de um aplicativo de doze fatores . Qualquer coisa que possa mudar entre ambientes de implantação – como identificadores de recursos para bancos de dados ou credenciais para serviços externos – deve ser extraída do código em variáveis ​​de ambiente.
    
    Mas nem sempre é prático definir variáveis ​​de ambiente em máquinas de desenvolvimento ou servidores de integração contínua onde vários projetos são executados. dotenv carrega variáveis ​​ENV de um arquivo .env quando o ambiente é inicializado.

--- settings/vars.php
+++ settings/vars.php
@@ -1,7 +1,14 @@
 <?php
+require ($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
+$dotenv->safeLoad();
+
 /*
-* Variáveis de ambiente
-*/
+ * Variáveis de ambiente
+ */
 $siteName = 'AIC - Administração de Igrejas Cristãs';
 $siteUrl = 'http://127.0.0.18';
 $siteEmail = 'francissantiago@lightburden.net';
@@ -9,9 +16,9 @@ $siteEmail = 'francissantiago@lightburden.net';
 /**
  * Variáveis de E-mail
  */
-$mailHost = '';
-$mailUsername = '';
-$mailPassword = '';
+$mailHost = $_ENV['MAIL_HOST'];
+$mailUsername = $_ENV['MAIL_USERNAME'];
+$mailPassword = $_ENV['MAIL_PASSWORD'];
 $mailSMTPSecure = 'tls';
 $mailPort = 587;
 $mailFrom = 'noreply@lightburden.net';
