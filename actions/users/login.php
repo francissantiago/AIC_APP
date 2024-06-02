8d90ec647f9da41f935bb92d63d7201ae08a42d8
Francis Santiago <francis1505@gmail.com>
2024-06-02 13:00:55 -0300

    [FEAT] Estrutura de login - viewes & actions + template de e-mail de reenvio de token de registro

---
+++ actions/users/login.php
@@ -0,0 +1,86 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+
+$msg = [];
+/* Validar método de acesso ao arquivo */
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (
+        isset(
+        $_POST['login_user_val'],
+        $_POST['login_password_val']
+    )
+    ) {
+        require_once ($_SERVER['DOCUMENT_ROOT'] . '/settings/dbConnection.php');
+        require_once ($_SERVER['DOCUMENT_ROOT'] . '/controllers/users/UsersController.php');
+        // Atribuição dos dados recebidos em variáveis
+        $userDoc = $_POST['login_user_val'];
+        $userPassword = $_POST['login_password_val'];
+
+        // Formatação dos campos
+        $userDocNoFormating = str_replace(['.', '-'], '', $userDoc);
+
+        // Cadastro do usuário
+        $conn = dbConnection();
+        $getClass = new UsersController($conn);
+
+        $registerUser = $getClass->loginUsr($userDocNoFormating, $userPassword);
+        $msg = $registerUser;
+
+    } else {
+        // Verifica os campos ausentes
+        $missingFileds = [];
+        if (!isset($_POST['userName'])) {
+            $missingFileds[] = 'Nome';
+        }
+        if (!isset($_POST['userDoc'])) {
+            $missingFileds[] = 'CPF';
+        }
+        if (!isset($_POST['userBirth'])) {
+            $missingFileds[] = 'Data de Nascimento';
+        }
+        if (!isset($_POST['userEmail'])) {
+            $missingFileds[] = 'E-mail';
+        }
+        if (!isset($_POST['userPassword'])) {
+            $missingFileds[] = 'Senha';
+        }
+        if (!isset($_POST['userRepeatPassword'])) {
+            $missingFileds[] = 'Repetir a Senha';
+        }
+        if (!isset($_POST['policyCheckbox'])) {
+            $missingFileds[] = 'Política de Usuário';
+        }
+        if (!isset($_POST['termCheckbox'])) {
+            $missingFileds[] = 'Termos e Condições de Usuário';
+        }
+
+        // Relaciona os campos ausentes e retorna o erro com dados mais completos sobre os campos ausentes
+        $msg = [
+            'code' => 400,
+            'message' => 'Existe(m) campo(s) ausente(s) no formulário! Por favor corrija o(s) campo(s): ' . implode(', ', $missingFields)
+        ];
+    }
+
+} else {
+    // Coleta informações sobre o autor do acesso
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $timestamp = date("Y-m-d H:i:s");
+    $url = $_SERVER['REQUEST_URI'];
+    $browser_info = get_browser(null, true);
+
+    // Retorna erro ao receber uma tentativa de acesso direto ao arquivo
+    $msg = [
+        'code' => 500,
+        'message' => [
+            'IP:' => $ip,
+            'URL' => $url,
+            'BROWSER:' => $browser_info,
+            'DATE:' => $timestamp
+        ],
+        'error_log:' => 'Tentativa de acesso negada! Dados coletados e armazenados!'
+    ];
+}
+
+echo json_encode($msg);
+
+?>
--- public/login.php
+++ public/login.php
@@ -1,6 +1,15 @@
 <?php
 $path = $_SERVER['DOCUMENT_ROOT'];
-include_once ($path.'/public/partials/header.php');
+include_once ($path . '/public/partials/header.php');
+// Verifica o status atual da sessão
+if (session_status() === PHP_SESSION_NONE) {
+    // Inicia a sessão se não estiver ativa
+    session_start();
+}
+
+if(isset($_SESSION['session_user_id']) && isset($_SESSION['session_user_doc']) && isset($_SESSION['session_user_email'])){
+	header('location: dashboard');
+}
 ?>
 <!-- App Capsule -->
 <div id="appCapsule">
@@ -9,7 +18,7 @@ include_once ($path.'/public/partials/header.php');
 		<h4>Preencha o formulário para entrar</h4>
 	</div>
 	<div class="section mb-5 p-2">
-		<form action="index.html">
+		<form>
 			<div class="card">
 				<div class="card-body pb-1">
 					<div class="form-group basic">
@@ -41,7 +50,7 @@ include_once ($path.'/public/partials/header.php');
 				<div><a href="forgotPass" class="text-muted">Esqueceu a senha?</a></div>
 			</div>
 			<div class="form-button-group  transparent">
-				<button type="submit" class="btn btn-primary btn-block btn-lg">Entrar</button>
+				<button type="submit" class="btn btn-primary btn-block btn-lg" id="login_btn">Entrar</button>
 			</div>
 		</form>
 	</div>
@@ -49,22 +58,102 @@ include_once ($path.'/public/partials/header.php');
 <!-- * App Capsule -->
 <!-- ========= JS Files =========  -->
 <!-- Bootstrap -->
-<script src="<?php $path;?>/assets/js/lib/bootstrap.bundle.min.js"></script>
+<script src="<?php $path; ?>/assets/js/lib/bootstrap.bundle.min.js"></script>
 <!-- Ionicons -->
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 <!-- Splide -->
-<script src="<?php $path;?>/assets/js/plugins/splide/splide.min.js"></script>
+<script src="<?php $path; ?>/assets/js/plugins/splide/splide.min.js"></script>
 <!-- Base Js File -->
-<script src="<?php $path;?>/assets/js/base.js"></script>
+<script src="<?php $path; ?>/assets/js/base.js"></script>
 <!-- Custom JS -->
 <script type="text/javascript">
-// Focar no input de login ao carregar a tela e formatar CPF
-$(document).ready(function(){
-	$('#userLogin').focus();
-	var mask_login = $('#userLogin');
-	mask_login.focus();
-	mask_login.mask('999.999.999-99', {reverse: true});
-});
+	// Focar no input de login ao carregar a tela e formatar CPF
+	$(document).ready(function () {
+		var login_user = $('#userLogin');
+		var login_password = $('#userPassword');
+		var login_btn = $('#login_btn');
+
+		login_user.focus();
+
+		login_user.focus();
+		login_user.mask('999.999.999-99', { reverse: true });
+
+		login_btn.click((e) => {
+			e.preventDefault();
+
+			let login_user_val = login_user.val();
+			let login_password_val = login_password.val();
+
+			if (!login_user_val) {
+				Swal.fire({
+					title: "Ops!",
+					html: "Preencha o campo <b>CPF</b>!",
+					icon: "warning",
+					showConfirmButton: false,
+					timer: 2000
+				}).then(() => {
+					login_user.focus();
+				});
+			} else if (!login_password_val) {
+				Swal.fire({
+					title: "Ops!",
+					html: "Preencha o campo <b>Senha</b>!",
+					icon: "warning",
+					showConfirmButton: false,
+					timer: 2000
+				}).then(() => {
+					login_user.focus();
+				});
+			} else {
+				$.ajax({
+					url: 'actions/users/login.php',
+					type: 'POST',
+					dataType: 'json',
+					data: {
+						login_user_val: login_user_val,
+						login_password_val: login_password_val
+					},
+					success: function (response) {
+						let jsonResponse = JSON.parse(JSON.stringify(response));
+						console.log(jsonResponse);
+						if (jsonResponse.code === 200) {
+							Swal.fire({
+								title: "Acesso Concedido!",
+								html: jsonResponse.message,
+								icon: "success",
+								showConfirmButton: true,
+								timer: 2000
+							}).then(function () {
+								window.location.href = "login";
+							});
+						} else {
+							Swal.fire({
+								title: "Acesso Negado!",
+								html: jsonResponse.message,
+								icon: "error",
+								showConfirmButton: true,
+								timer: 5000
+							});
+						}
+					},
+					error: function (e) {
+						Swal.fire({
+							title: "Erro!",
+							html: e.message,
+							icon: "error",
+							showConfirmButton: true,
+							timer: 5000
+						});
+						console.log(e);
+					}
+				});
+			}
+
+
+		});
+
+	});
 </script>
 </body>
+
 </html>
