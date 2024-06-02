dc328e093c344512d4f8b9d5a05a369ed30bd71b
Francis Santiago <francis1505@gmail.com>
2024-06-02 13:01:14 -0300

    [FIX] Validação de sessão criada e redirecionamentos

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	if (session_status() == PHP_SESSION_NONE) {
 		session_start();
 	}
-	if (!isset($_SESSION['userLogin'])) {
+	if(!isset($_SESSION['session_user_id']) && !isset($_SESSION['session_user_doc']) && !isset($_SESSION['session_user_email'])){
 		header('location: login');
 	} else {
 		header('location: dashboard');
