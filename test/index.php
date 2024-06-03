691752fe3bbfd5d5d6acd715582b802fffb01b31
Francis Santiago <francis1505@gmail.com>
2024-06-03 11:55:57 -0300

    [TEST] Estrutura de testes com controladores

--- test/index.php
+++ test/index.php
@@ -1,27 +1,94 @@
 <?php
-require_once ($_SERVER['DOCUMENT_ROOT'] . '/settings/vars.php');
-require_once ($_SERVER['DOCUMENT_ROOT'] . '/settings/dbConnection.php');
-require_once ($_SERVER['DOCUMENT_ROOT'] . '/controllers/general/GeneralController.php');
-require_once ($_SERVER['DOCUMENT_ROOT'] . '/controllers/general/MailerController.php');
+//header('Content-Type: text/html; charset=utf-8');
 
-class TestsController extends GeneralController
-{
-    private $conn;
-
-    public function __construct($conn)
-    {
-        $this->conn = $conn;
-    }
-
-    public function main()
-    {
-
-    }
-}
 $msg = [];
-$conn = dbConnection();
-$callClass = new TestsController($conn);
-$callMethod = $callClass->main();
+/* Validar método de acesso ao arquivo */
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (
+        isset(
+        $_GET['user_id'],
+        $_GET['user_token']
+    ) &&
+        !empty([
+            $_GET['user_id']
+        ]) &&
+        !empty(
+        $_GET['user_token']
+    )
+    ) {
+        require_once ($_SERVER['DOCUMENT_ROOT'] . '/settings/dbConnection.php');
+        require_once ($_SERVER['DOCUMENT_ROOT'] . '/controllers/general/GeneralController.php');
+        require_once ($_SERVER['DOCUMENT_ROOT'] . '/test/controllers.php');
 
-echo json_encode($callMethod);
+        // Atribuição dos dados recebidos em variáveis
+        $field_01 = $_GET['user_id'];
+        $field_02 = $_GET['user_token'];
+
+        // Cadastro do usuário
+        $conn = dbConnection();
+        $getClass = new TestsController($conn);
+        $getGeneralClass = new GeneralController($conn);
+
+        $response = $getClass->validateRegisterToken($field_01, $field_02, $getGeneralClass);
+        if ($response['code'] === 200) {
+            $templateHTML = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/terminal_template.php');
+            $meta_tag = '<meta http-equiv="refresh" content="10; url=/login">';
+            $redirect_time = '10 segundos';
+            $replacedHTML = str_replace(['PHP_TITLE_CODE', 'PHP_MESSAGE', 'PHP_TIME', 'PHP_META_TAG'], [$response['code'], $response['message'], $redirect_time, $meta_tag], $templateHTML);
+
+            echo $replacedHTML;
+            exit;
+        }
+
+        $msg = $response;
+
+    } else {
+        // Verifica os campos ausentes
+        $missingFields = [];
+        if (!isset($_GET['user_id']) || empty($_GET['user_id'])) {
+            $missingFields[] = 'FIELD_01';
+        }
+        if (!isset($_GET['user_token']) || empty($_GET['user_token'])) {
+            $missingFields[] = 'FIELD_02';
+        }
+
+        // Relaciona os campos ausentes e retorna o erro com dados mais completos sobre os campos ausentes
+        // $msg = [
+        //     'code' => 400,
+        //     'message' => 'Existe(m) campo(s) ausente(s) no formulário! Por favor corrija o(s) campo(s): ' . implode(', ', $missingFields)
+        // ];
+
+        $templateHTML = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/terminal_template.php');
+            $meta_tag = '<meta http-equiv="refresh" content="10; url=/login">';
+            $code = 400;
+            $message = 'Existe(m) campo(s) ausente(s) no formulário! Por favor corrija o(s) campo(s): ' . implode(', ', $missingFields);
+            $redirect_time = '10 segundos';
+            $replacedHTML = str_replace(['PHP_TITLE_CODE', 'PHP_MESSAGE', 'PHP_TIME', 'PHP_META_TAG'], [$code, $message, $redirect_time, $meta_tag], $templateHTML);
+
+            echo $replacedHTML;
+            exit;
+    }
+
+} else {
+    // Coleta informações sobre o autor do acesso
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $timestamp = date("Y-m-d H:i:s");
+    $url = $_SERVER['REQUEST_URI'];
+    $browser_info = get_browser(null, true);
+
+    // Retorna erro ao receber uma tentativa de acesso direto ao arquivo
+    $msg = [
+        'code' => 500,
+        'message' => [
+            'IP:' => $ip,
+            'URL' => $url,
+            'BROWSER:' => $browser_info,
+            'DATE:' => $timestamp
+        ],
+        'error_log:' => 'Tentativa de acesso negada! Dados coletados e armazenados!'
+    ];
+}
+
+echo json_encode($msg);
+
 ?>
